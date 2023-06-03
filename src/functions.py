2de624ed98014240705086ff70743f0e92daa6ca
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2023-06-03 19:38:15 +0300

    created conftest.py wrote func and tests for load_data_from_json, delete_empty_operation

--- src/functions.py
+++ src/functions.py
@@ -1,17 +1,22 @@
-def load_data_from_json():
+import json
+
+
+def load_data_from_json(filename):
 	"""
 	Считывает информация с json-файла и возвращает ее
 	:return: Список данных
 	"""
-	pass
+	with open(filename, 'r', encoding='utf-8') as f:
+		data = json.load(f)
+	return data
 
 
-def delete_empty_operation():
+def delete_empty_operation(operations_list):
 	"""
 	Удаляет пустые записи об операциях из данных
 	:return: Список с заполненными данными
 	"""
-	pass
+	return [operation for operation in operations_list if operation != {}]
 
 
 def sort_data_by_date():
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,12 +1,12 @@
 from src.functions import *
 
 
-def test_load_data_from_json():
-	pass
+def test_load_data_from_json(path_to_test_json, data_from_test_json):
+	assert load_data_from_json(path_to_test_json) == data_from_test_json
 
 
-def test_delete_empty_operation():
-	pass
+def test_delete_empty_operation(data_for_delete_empty_operation, expected_result_for_delete_empty_operation):
+	assert delete_empty_operation(data_for_delete_empty_operation) == expected_result_for_delete_empty_operation
 
 
 def test_sort_data_by_date():
