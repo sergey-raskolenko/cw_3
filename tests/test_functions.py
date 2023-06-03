24515d54c870d128c654deaa669686f792512269
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2023-06-03 18:55:20 +0300

    create tests package with test_functions.py

---
+++ tests/test_functions.py
@@ -0,0 +1,17 @@
+from src.functions import *
+
+
+def test_load_data_from_json():
+	pass
+
+
+def test_delete_empty_operation():
+	pass
+
+
+def test_sort_data_by_date():
+	pass
+
+
+def test_get_executed_data():
+	pass
